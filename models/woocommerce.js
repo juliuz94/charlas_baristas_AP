a367ebfef1dc6f1b5a0905f8d9357d01f16c3535
Julian L <julianlozano@Julians-MacBook-Air.local>
2020-06-04 12:07:56 -0500

    heroku deply try 2

--- models/woocommerce.js
+++ models/woocommerce.js
@@ -3,7 +3,7 @@ const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 const woocommerce = new WooCommerceRestApi({
   url: "https://www.cafeamorperfecto.com",
   consumerKey: "ck_fd34be9f5745faf54062c49f44414f3cce6c7044",
-  consumerSecret: process.env.WOOSECRET,
+  consumerSecret: 'cs_3afac13f39577ae7a38f70773b12e64719702d9c',
   version: "wc/v3"
 });
 
