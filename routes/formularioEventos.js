d3f86bf854877eda85d31211801bfdd44f435ac7
Julian L <julianlozano@Julians-MacBook-Air.local>
2020-07-03 17:03:18 -0500

    se ajusta la landing de eventos para el conversatorio Tinto con Tinto

--- routes/formularioEventos.js
+++ routes/formularioEventos.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
         name: req.body.name,
         email: req.body.email,
         phoneNumber: req.body.telephone,
-        event: 'Conversatorio Día Nacional del Café'
+        event: 'Tinto con tinto'
     });
     newInscrito.save();
 
